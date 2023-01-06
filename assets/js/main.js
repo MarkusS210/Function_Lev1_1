0a4bb251eacfbb1b8531538da6895cc829d298fd
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-06 15:19:13 +0100

    neueVersion

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,7 @@ function into () {
     let a = (1 + 3)
     let wellcome = "Hello World"
     console.log(wellcome)
-    let result = "1+3=" + a
-    console.log(result)
+    console.log("1+3=" + a);
 }
 
 into()
